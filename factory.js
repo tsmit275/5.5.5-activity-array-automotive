261aa88d26098d87d0a28ce1ef1df0243a00be8e
tsmit275 <taylor.lsmith03@gmail.com$ git config --global user.taylor.lsmith03@gmail.com>
2023-09-26 19:47:55 -0400

    FINAL

--- factory.js
+++ factory.js
@@ -1,3 +1,22 @@
 class Factory{
-    
+    constructor (location, owner){
+    this.location = location
+    this.owner = owner
+    //default for a facory is 0 vehicles 
+    this.vehicles = []
+   }
+  //create vehcile is not a function 
+  createVehicle(vehicle){
+    this.vehicles.push(vehicle)
+  }
+  //its paints the matching vehicle the specified color
+  paintVehicle(vin, paintColor){
+    //index of the vehicle with that vin
+  let vIndex = this.vehicles.findIndex(v => v.vin === vin)
+  this.vehicles[vIndex].paint(paintColor)
+  }
+  //when called lists all vehicles in factory 
+  listVehicles(){
+  return this.vehicles
+  }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,19 @@
 class Vehicle {
+    constructor(make, model, year, price, vin){
+    this.make = make
+    this.model = model
+    this.year = year
+    this. price = price
+    this.vin = vin
+    this.color = undefined 
+    }
+    drive(){
+        return this.year + " "+ this.make + " " + this.model+ " "+ "drives"
+     }
+    paint(color){
+    this.color = color
     
+    }
+
 }
 
